6755661217d46977bc4bb5f8b46a523d36d5dff0
kyu_repo <khkhong1@gmail.com>
2019-05-01 16:38:29 +0900

    recyclerview 실습 코드 추가

---
+++ study_proj/Khk_FragmentPractice/app/src/main/java/com/example/common/Constants.kt
@@ -0,0 +1,5 @@
+package com.example.common
+
+object Constants{
+    const val VAL_INTENT_NAME = "STRINGLISTFROMFILE"
+}
--- study_proj/Khk_FragmentPractice/app/src/main/java/com/example/fileioPractice/FileActivity.kt
+++ study_proj/Khk_FragmentPractice/app/src/main/java/com/example/fileioPractice/FileActivity.kt
@@ -1,91 +1,76 @@
 package com.example.fileioPractice
 
+import android.Manifest
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Environment
-import android.os.StrictMode
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import com.example.fragmentpractice.R
-import khttp.post
-import java.io.BufferedWriter
-import java.io.File
-import java.io.FileWriter
-import java.nio.Buffer
+import java.io.*
+import android.content.pm.PackageManager
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
+import com.example.common.Constants
 
 class FileActivity : AppCompatActivity() {
-
-    // Root Directory 상수 선언
-    //private val fileLocation : String = Environment.getRootDirectory().absolutePath
-    private val fileLocation : String = Environment.DIRECTORY_DOWNLOADS
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_file)
 
-        val openButton = findViewById<Button>(R.id.saveButton)
-        val editedString = findViewById<EditText>(R.id.editText)
+        try {
 
-        openButton.setOnClickListener{ fileSave(editedString.text.toString()) }
+            val openButton = findViewById<Button>(R.id.saveButton)
+            val editedString = findViewById<EditText>(R.id.editText)
 
-    }
-
-    /**
-     *
-     * 파일을 저장할 디렉토리를 설정한다.
-     *
-     * @author khk
-     *
-     * */
-    private fun openFileBrowser()  {
-
-        var selectedSavePath : String ?= null
-
-        try{
-            val fileList = File(fileLocation).listFiles()
+            openButton.setOnClickListener{ fileSave(editedString.text.toString()) }
 
         }
-        catch(ex:Exception){
-            // 어떤 에러가 발생했는지 확인
+        catch(ex: Exception){
             ex.printStackTrace()
-            Toast.makeText(this, ex.message, Toast.LENGTH_LONG)
-        }
-        finally {
-            // 파일 브라우저 닫기
-            // file I/O 관련한 내용 닫기
+            Toast.makeText(applicationContext, ex.message, Toast.LENGTH_LONG)
         }
     }
-
     /**
      *
-     * EditText에 적힌 내용을 파일에 저장한다.
+     * EditText에 적힌 내용을 파일에 저장 후 RecyclerViewActivity로 넘긴다.
      *
      * @author khk
      * */
     private fun fileSave(text:String){
         // 저장 경로
+        val fileLocation : String = filesDir.toString()
         try{
-            var saveFile = File(fileLocation)
-            if(!saveFile.exists()){
-                throw Exception("There is no Directory!")
-            }
 
-            var writer = BufferedWriter(FileWriter(saveFile.name + "/fileIOPractice.txt",true));
-            writer.append(text)
-            writer.close()
+            var fileWriter = FileWriter(fileLocation + "fileIOPractice.txt", false)
+            var bufferedWriter = BufferedWriter(fileWriter)
+            bufferedWriter.write(text)
+            bufferedWriter.close()
         }
         catch(ex:Exception){
             // 어떤 에러가 발생했는지 확인
             ex.printStackTrace()
-            Toast.makeText(this, ex.message, Toast.LENGTH_LONG)
-        }
-        finally {
-            // 파일 브라우저 닫기
-            // file I/O 관련한 내용 닫기
+            Toast.makeText(applicationContext, ex.message, Toast.LENGTH_LONG).show()
         }
 
+        try{
+            var fileLineList : List<String>?= null
 
+            var fileReader = FileReader (fileLocation + "fileIOPractice.txt")
+            var bufferedReader = BufferedReader(fileReader)
+
+            fileLineList = bufferedReader.readLines()
+
+            var intent = Intent(this, RecyclerViewActivity::class.java)
+            intent.putExtra(Constants.VAL_INTENT_NAME, ArrayList(fileLineList))
+            startActivity(intent)
+        }
+        catch(ex:Exception){
+            ex.printStackTrace()
+            Toast.makeText(applicationContext, ex.message, Toast.LENGTH_LONG).show()
+        }
     }
 }
