9c8610c83740cae258fe8c8edf13260fabe892b3
ChoiHyeongHo <choi902307@gmail.com>
2019-05-01 17:03:59 +0900

    no message

---
+++ study_proj/Ch1/app/src/main/java/com/stu/sample/CHH/MyAdapter.java
@@ -0,0 +1,53 @@
+package com.chh.hw1;
+
+import android.support.v7.widget.RecyclerView.Adapter;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class MyAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>  {
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        ImageView imageView;
+        TextView textView;
+
+        ViewHolder(View view){
+            super(view);
+            imageView = view.findViewById(R.id.image);
+            textView = view.findViewById(R.id.textView);
+        }
+    }
+
+    private ArrayList<itemData> itemData;
+    MyAdapter(ArrayList<itemData> itemData){
+        this.itemData = itemData;
+    }
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.item, parent, false);
+
+        return new ViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+
+        ViewHolder ViewHolder = (ViewHolder) holder;
+
+        ViewHolder.imageView.setImageResource(itemData.get(position).num);
+        ViewHolder.textView.setText(itemData.get(position).text);
+    }
+
+    @Override
+    public int getItemCount() {
+        return itemData.size();
+    }
+}
+
---
+++ study_proj/Ch1/app/src/main/java/com/stu/sample/CHH/itemData.java
@@ -0,0 +1,14 @@
+package com.chh.hw1;
+
+import android.graphics.drawable.Drawable;
+
+public class itemData {
+    public String text;
+    public int num;
+
+    public itemData(String text, int num) {
+        this.text = text;
+        this.num = num;
+    }
+
+}
