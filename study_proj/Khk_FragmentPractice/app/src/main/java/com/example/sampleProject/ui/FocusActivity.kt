f69fabd615258b4349bb6708a2fd19d9abe40f54
librekyu <khkhong1@gmail.com>
2019-06-16 23:54:20 +0900

    item별 클릭 이벤트 관련 코드 추가

--- study_proj/Khk_FragmentPractice/app/src/main/java/com/example/sampleProject/ui/FocusActivity.kt
+++ study_proj/Khk_FragmentPractice/app/src/main/java/com/example/sampleProject/ui/FocusActivity.kt
@@ -4,12 +4,17 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.DividerItemDecoration
 import android.support.v7.widget.LinearLayoutManager
+import android.widget.Toast
 import com.example.sampleProject.R
 import com.example.sampleProject.ui.adapter.FocusAdapter
 import kotlinx.android.synthetic.main.activity_recycler_view.*
 import java.util.stream.Collectors.toList
 
-class FocusActivity:AppCompatActivity(){
+class FocusActivity : AppCompatActivity(), FocusAdapter.RecyclerViewClickListener{
+    override fun onFocusButtonClicked(pos: Int) {
+        Toast.makeText(this, "clicked $pos item ", Toast.LENGTH_SHORT).show()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -19,12 +24,16 @@ class FocusActivity:AppCompatActivity(){
         var stringList: List<String> = listOf("1","2","3","4","5","6","7","8","9","10")
 
         val adapter = FocusAdapter(this, stringList)
+
+        adapter.setOnClickListener(this)
         mRecyclerView.adapter =  adapter
+
 
         val linearLayoutManager = LinearLayoutManager(this)
         mRecyclerView.layoutManager = linearLayoutManager
         mRecyclerView.setHasFixedSize(true)
         mRecyclerView.addItemDecoration(DividerItemDecoration(this, DividerItemDecoration.VERTICAL))
+
     }
 
     override fun onDestroy() {
--- study_proj/Khk_FragmentPractice/app/src/main/java/com/example/sampleProject/ui/adapter/FocusAdapter.kt
+++ study_proj/Khk_FragmentPractice/app/src/main/java/com/example/sampleProject/ui/adapter/FocusAdapter.kt
@@ -1,26 +1,41 @@
-package com.example.sampleProject.ui.adapter
+package  com.example.sampleProject.ui.adapter
 
 import android.content.Context
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 import com.example.sampleProject.R
+import org.w3c.dom.Text
 
 class FocusAdapter constructor(context: Context, items : List<String>) : RecyclerView.Adapter<FocusViewHolder>(){
 
     private var context : Context = context
     private var items : List<String> = items
+    private lateinit var mListner : RecyclerViewClickListener
+
+    interface RecyclerViewClickListener{
+        fun onFocusButtonClicked(pos : Int)
+    }
+
+    fun setOnClickListener(listener : RecyclerViewClickListener){
+        mListner = listener
+    }
 
     override fun onBindViewHolder(holder: FocusViewHolder, position: Int) {
         holder?.bind(items[position], context)
 
-        val itemData = items[position]`
-        holder.itemView.setOnClickListener { View.OnClickListener {
-            Toast.makeText(context, "$position", Toast.LENGTH_SHORT).show()
-        }}
+        if(mListner != null) {
+            holder.focusButton.setOnClickListener (object : View.OnClickListener {
+                override fun onClick(v: View?) {
+                    mListner.onFocusButtonClicked(position)
+                }
+
+            })
+        }
 
     }
 
@@ -37,9 +52,11 @@ class FocusAdapter constructor(context: Context, items : List<String>) : Recycle
  * @author khk
  * */
 class FocusViewHolder constructor(itemView : View) : RecyclerView.ViewHolder(itemView){
-    private var item = itemView?.findViewById<TextView>(R.id.EditTextFocus)
+
+    var editTextView = itemView?.findViewById<TextView>(R.id.EditTextFocus)
+    var focusButton = itemView?.findViewById<Button>(R.id.focusButton)
 
     fun bind(line: String, context: Context){
-        item?.text = line
+        editTextView?.text = line
     }
 }
